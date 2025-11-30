eeb65b54394d43013bab44bd180cce1f03e4f57d
Abu <aburiza.21@gmail.com>
2025-11-30 12:43:22 +0700

    Set_Migration

---
+++ database/migrations/2025_11_30_053541_create_transactions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('invoice')->unique();
+            $table->decimal('total', 10, 2);
+            $table->decimal('pay', 10, 2);
+            $table->decimal('return', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
